f12523aa15a97c75e42f1f6be12c1708d14a3bd4
Francko101 <franckolamanilao@gmail.com>
2026-04-15 17:47:01 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+// NOTE: Your full App.jsx code is already in ChatGPT canvas.
+// If needed, paste it here manually.
+
+export default function App() {
+  return <div>Paste your App.jsx code here</div>;
+}
